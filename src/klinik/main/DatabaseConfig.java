7219219fa984b496c56041eada3547920584b1a3
JoiceNapitupulu <145031687+JoiceNapitupulu@users.noreply.github.com>
2026-05-05 08:59:55 +0700

    edit databaseconfig

--- src/klinik/main/DatabaseConfig.java
+++ src/klinik/main/DatabaseConfig.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     // Sesuaikan kredensial PostgreSQL Anda di sini
-    private static final String URL = "jdbc:postgresql://localhost:5432/clinic_care_hub";
-    private static final String USER = "postgres"; 
-    private static final String PASSWORD = "password"; 
+    private static final String URL = "jdbc:postgresql://localhost:5432/db_klinik";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
